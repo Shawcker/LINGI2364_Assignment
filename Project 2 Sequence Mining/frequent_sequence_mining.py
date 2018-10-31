ca27475e64cb73918c410ab112820242c25dc86d
xjyssg <jiayuexue30@gmail.com>
2018-10-31 20:06:30 +0100

    dataset

---
+++ Project 2 Sequence Mining/frequent_sequence_mining.py
@@ -0,0 +1,42 @@
+import os
+
+class Dataset:
+    """Utility class to manage a dataset stored in a external file."""
+
+    def __init__(self, filepath):
+        """reads the dataset file and initializes files"""
+        self.transactions = list()
+        self.items = set()
+
+        try:
+            lines = [line.strip() for line in open(filepath, "r")]
+            lines = [line for line in lines if line]  # Skipping blank lines
+            for line in lines:
+                transaction = list(map(int, line.split(" ")))
+                self.transactions.append(transaction)
+                for item in transaction:
+                    self.items.add(item)
+        except IOError as e:
+            print("Unable to read dataset file!\n" + e)
+
+    def trans_num(self):
+        """Returns the number of transactions in the dataset"""
+        return len(self.transactions)
+
+    def items_num(self):
+        """Returns the number of different items in the dataset"""
+        return len(self.items)
+
+    def get_transaction(self, i):
+        """Returns the transaction at index i as an int array"""
+        return self.transactions[i]
+
+
+pwd = os.getcwd()
+Dataset_Path = "Datasets"
+Subpath = "Reuters"
+Dataset_Name = "acq.txt"
+Final_Path = os.path.join(pwd, Dataset_Path, Subpath, Dataset_Name)
+print(Final_Path)
+data = Dataset(Final_Path)
+print(data.trans_num())
